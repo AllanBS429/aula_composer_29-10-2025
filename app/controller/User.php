b2f319c5cccf7712f2859c3bd04366149746ee4e
AllanBS429 <allbatista@hotmail.com>
2025-11-13 20:32:00 -0400

    ok

--- app/controller/User.php
+++ app/controller/User.php
@@ -4,23 +4,24 @@ namespace app\controller;
 
 class User extends Base
 {
+
     public function lista($request, $response)
     {
         $dadosTemplate = [
-            'titulo' => 'Lista de usuarios',
+            'titulo' => 'Lista de usuário'
         ];
         return $this->getTwig()
             ->render($response, $this->setView('listuser'), $dadosTemplate)
             ->withHeader('Content-Type', 'text/html')
             ->withStatus(200);
     }
-     public function cadastro($request, $response)
+    public function cadastro($request, $response)
     {
         $dadosTemplate = [
-            'titulo' => 'Lista de usuarios',
+            'titulo' => 'Cadastro de usuário'
         ];
         return $this->getTwig()
-            ->render($response, $this->setView('listuser'), $dadosTemplate)
+            ->render($response, $this->setView('user'), $dadosTemplate)
             ->withHeader('Content-Type', 'text/html')
             ->withStatus(200);
     }
--- html/index.php
+++ html/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use Slim\Factory\AppFactory;
-use app\database\db\builder\Builder;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -10,8 +9,8 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
 require __DIR__ . '/../app/helper/settings.php';
 require __DIR__ . '/../app/route/route.php';
 
 $app->run();
- 
